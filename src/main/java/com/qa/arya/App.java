e1b40e7dd31d9c170c3448a8ec6fa81c0200dc24
Himanshu <himanshuarya545@gmail.com>
2024-07-24 18:45:40 +0530

    first commit

---
+++ src/main/java/com/qa/arya/App.java
@@ -0,0 +1,12 @@
+package com.qa.arya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(10 + 20);
+    }
+}
